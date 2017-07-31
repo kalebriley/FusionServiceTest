bd36b5b2465ec7ad2f847a7d4d63128fff578a71
kaleb riley <kaleb.riley@robotsandpencils.com>
2017-07-31 15:26:21 -0600

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by kaleb riley on 7/31/17.
+//  Copyright © 2017 kaleb riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
